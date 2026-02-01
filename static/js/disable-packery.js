61254d795ed843f5c92753cfbe25bba8d6b40331
matteodimauro <matteo.dm@live.com>
2026-02-01 21:56:31 +0000

    Disable medium-zoom for gallery images to prevent distortion

--- static/js/disable-packery.js
+++ static/js/disable-packery.js
@@ -3,7 +3,7 @@
   // Completely kill Packery
   window.Packery = null;
   
-  // When page loads, clean up any Packery instances
+  // When page loads, clean up any Packery instances and disable medium-zoom for gallery images
   window.addEventListener('load', function() {
     // Remove all Packery styling
     document.querySelectorAll('.gallery').forEach(function(gallery) {
@@ -11,13 +11,19 @@
       gallery.style.width = '';
       gallery.style.height = '';
       
-      // Clean gallery items
+      // Clean gallery items and disable medium-zoom
       Array.from(gallery.children).forEach(function(item) {
         item.style.position = '';
         item.style.left = '';
         item.style.top = '';
         item.style.width = '';
         item.style.height = '';
+      });
+      
+      // Disable medium-zoom for gallery images
+      gallery.querySelectorAll('img').forEach(function(img) {
+        img.classList.add('nozoom');
+        img.removeAttribute('data-medium-zoom-image');
       });
     });
   }, true);
